cf82d2a81e711913d180849f026bfb2a79ddd9bc
Jolanda Besteman <jebesteman@gmail.com>
2021-12-01 18:28:36 +0100

    comments opdracht  pass

--- comments/main.py
+++ comments/main.py
@@ -3,3 +3,13 @@ __winc_id__ = '63ce21059cf34d3d8ffef497ede7e317'
 __human_name__ = 'comments'
 
 # Add your code after this line
+
+# Dit is de eerste single-line comment
+# Dit is de tweede single-line comment
+question = "wat gaan we vanavond doen?" # eerste end-of-line comment
+answer = "Proberen wat te relaxen!"     # tweede end-of-line comment
+"""Als laatste de eerste multiline comment.
+Het is echt even wennen dat Python """
+
+"""Dit is de tweede multiline comment.
+Wel leuk maar beetje verwarrend nog als je JS gewend ben"""
